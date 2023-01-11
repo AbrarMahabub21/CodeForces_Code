821a90515658e8e7d996d4d5a0bb337eb2f6fbdf
Abrar Mahabub (Nowrid) <abrar.nowrid@gmail.com>
2023-01-12 00:48:42 +0600

    Added to CodeForces

---
+++ 266B - Queue at the School/main.cpp
@@ -0,0 +1,26 @@
+#include <bits/stdc++.h>
+
+using namespace std;
+
+int main()
+{
+   int a,b;
+   string s;
+   cin>>a>>b;
+   cin>>s;
+   for (int i =0 ; i<b;i++)
+   {
+       for (int j = 0 ; j<s.size()-1;j++)
+       {
+           if (s[j] == 'B' && s[j+1] == 'G')
+           {
+               s[j]= 'G';
+               s[j+1] = 'B';
+               j++;
+           }
+       }
+   }
+   cout<<s;
+   cout<<endl;
+   return 0;
+}
