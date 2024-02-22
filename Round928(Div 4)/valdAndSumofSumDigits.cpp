6c796d14f504da0c4bd048de6d0e5b5a1220b8ce
Abrar Mahabub (Nowrid) <abrar.nowrid@gmail.com>
2024-02-22 10:18:20 +0600

    half done

---
+++ Round928(Div 4)/valdAndSumofSumDigits.cpp
@@ -0,0 +1,17 @@
+#include <bits/stdc++.h>
+using namespace std;
+
+int main()
+{
+    int t;
+    cin >> t;
+    while (t--)
+    {
+        int n, sum = 0;
+        cin >> n;
+        if (n < 10)
+        {
+            sum = (n * (n - 1)) / 2;
+        }
+    }
+}
